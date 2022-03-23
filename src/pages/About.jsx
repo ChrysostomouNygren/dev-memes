6bd84e3c352095959c86d4859ea6caa88c60e47c
Katerina Chrysostomou <katerina.chrysostomou@hotmail.com>
2022-03-23 09:27:18 +0100

    added some memez

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
+import Header from "../components/Header";
 
 function About() {
   return (
     <div>
-        <h2>About me and the project</h2>
-        <p>will come soon! enjoy the memez meanwhile :)</p>
+      <Header />
+      <h2>About me and the project</h2>
+      <p>will come soon! enjoy the memez meanwhile :)</p>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import Header from "../components/Header";
 import { memes } from "../components/memes.json";
 
-// Importera memes
+// randomize memes
 function Home() {
 
+  const randomMeme = memes[Math.floor(Math.random() * memes.length)]
+  
   return (
     <div>
       <Header />
       <main>
-        {memes.map((item, index) => (
+
+        {memes.map((item) => (
           <img src={item.url} />
         ))}
       </main>
