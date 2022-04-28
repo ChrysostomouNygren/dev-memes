4e36f96ca69b5eb7aa67df187265df7fba75834a
Katerina Chrysostomou Nygren <katerina.chrysostomou@hotmail.com>
2022-04-28 13:58:51 +0200

    changed some info and added link

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,12 +9,13 @@ function About() {
         <h2>About me and the project</h2>
       <span>
         <p>My name is Katerina, an aspiring frontend- & app developer based in Stockholm, Sweden.<br />
-        I've been working retail for over 10 years, and felt like it was time to try out something new.<br />
-        My previous educations includes lots of art and design, and trough a course in webdesign I was introduced to programming.</p>
+        Wan't some more of my sillyness or to hire me? Check out these links:
+        </p>
         <ul>
-          <li><a href="https://github.com/ChrysostomouNygren">Codes</a> (aka my github)</li>
-          <li><a href="https://www.instagram.com/kottlas.miniatures/">Art</a></li>
           <li><a href="https://www.linkedin.com/in/katerina-chrysostomou-nygren/">Linkedin</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="https://github.com/ChrysostomouNygren">Github</a></li>
+          <li><a href="https://www.instagram.com/kottlas.miniatures/">Art</a></li>
         </ul>
       </span>
       <Footer />
